f8d2aa4f59c2b46ef4a5790620c092a286ef30c9
Lu Weeke <luweike64@gmail.com>
2024-03-28 17:49:57 +0800

    add app model

--- pkg/routers/api/v1/article.go
+++ pkg/routers/api/v1/article.go
@@ -1,6 +1,11 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"blogserver/pkg/app"
+	"blogserver/pkg/errcode"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Article struct{}
 
@@ -8,7 +13,10 @@ func NewArticle() Article {
 	return Article{}
 }
 
-func (a Article) Get(c *gin.Context)    {}
+func (a Article) Get(c *gin.Context) {
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+	return
+}
 func (a Article) List(c *gin.Context)   {}
 func (a Article) Create(c *gin.Context) {}
 func (a Article) Update(c *gin.Context) {}
